329d39de840507701622283f629ca473eb65f6b9
Kelsey Hightower <kelsey.hightower@gmail.com>
2016-09-29 09:12:39 -0700

    use offical kubernetes release

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -7,9 +7,9 @@ kubectl --context=federation-cluster delete services nginx
 
 kubectl --namespace=federation delete pods,svc,rc,deployment,secret --all
 
-gcloud dns managed-zones delete federation
+gcloud dns managed-zones delete federation -q
 
-gcloud container clusters delete gce-asia-east1 --zone=asia-east1-b
-gcloud container clusters delete gce-europe-west1 --zone=europe-west1-b
-gcloud container clusters delete gce-us-central1 --zone=us-central1-b
-gcloud container clusters delete gce-us-east1 --zone=us-east1-b
+gcloud container clusters delete gce-asia-east1 -q --zone=asia-east1-b
+gcloud container clusters delete gce-europe-west1 -q --zone=europe-west1-b
+gcloud container clusters delete gce-us-central1 -q --zone=us-central1-b
+gcloud container clusters delete gce-us-east1 -q --zone=us-east1-b
